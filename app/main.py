e2ad5fb8420ec0020a19ce20e5d84a6f8d330e4b
Simon HARVEY <simon.harvey.86@gmail.com>
2024-10-28 15:22:49 -0400

    refactor: rework project structure (#32)
    * refactor: rework project structure
    
    * ci: rework strategy
    
    * ci: rework strategy

---
+++ app/main.py
@@ -0,0 +1,38 @@
+import contextlib
+import os
+from dotenv import dotenv_values
+from http.server import HTTPServer, BaseHTTPRequestHandler
+from time import sleep
+
+load_dotenv()
+
+HOST = "0.0.0.0"
+PORT = 8080
+MESSAGE_API = os.getenv('MESSAGE')
+
+
+class Handler(BaseHTTPRequestHandler):
+    def do_GET(self):
+        self.send_response(200)
+        self.send_header("Content-Type", "text/plain")
+        self.end_headers()
+
+        message = "Hello World!"
+
+        self.wfile.write(bytes(message, "utf-8"))
+
+
+def run(host, port, server_class=HTTPServer, handler_class=Handler):
+    server_address = (host, port)
+    httpd = server_class(server_address, handler_class)
+
+    with contextlib.suppress(KeyboardInterrupt):
+        httpd.serve_forever()
+
+    httpd.server_close()
+
+
+if __name__ == "__main__":
+    sleep(10)
+    print("Starting server...")
+    run(HOST, PORT)
